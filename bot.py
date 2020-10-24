3a1d58805f5d9623cbc38471053417fae9c6ffbb
akrish4 <ananthakrishnan20121999@gmail.com>
2020-10-24 17:09:33 +0530

    upload commit

---
+++ bot.py
@@ -0,0 +1,9 @@
+print('Hello! My name is Bilal.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+your_name = input() # reading a name
+# another way to print 
+#  print('What a great name you have, {}'. format(your_name)) 
+# another way to print
+#  print('What a great name you have, ',your_name) 
+print('What a great name you have, '+ your_name) 
