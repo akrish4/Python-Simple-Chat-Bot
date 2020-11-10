1607613400ec88b6f7617f4bf897ad0d91e26f53
akrish4 <ananthakrishnan20121999@gmail.com>
2020-11-10 20:15:47 +0530

    update commit

--- bot.py
+++ bot.py
@@ -7,3 +7,11 @@ your_name = input() # reading a name
 # another way to print
 #  print('What a great name you have, ',your_name) 
 print('What a great name you have, '+ your_name) 
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+# reading all remainders
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print("Your age is " , age ," that's a good time to start programming!" )
