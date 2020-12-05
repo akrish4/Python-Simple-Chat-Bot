fcd477451a92ce8dee88685824497ac538be3b8a
akrish4 <ananthakrishnan20121999@gmail.com>
2020-12-05 09:25:01 +0530

    updated stage 4

---
+++ bot/bot.py
@@ -0,0 +1,20 @@
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+name = input()
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+rem3 = int(input())
+rem5 = int(input())
+rem7 = int(input())
+age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+# read a number and count to it here
+count = int(input())
+i = 0
+while i <= count:
+    print(str(i) + " !")
+    i += 1
+print('Completed, have a nice day!')
