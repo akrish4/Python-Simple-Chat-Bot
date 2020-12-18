44ceb30959201dc64c39aa058aade6c5def47a35
akrish4 <ananthakrishnan20121999@gmail.com>
2020-12-18 10:33:10 +0530

    updated commit

--- bot/bot.py
+++ bot/bot.py
@@ -1,20 +1,60 @@
-print('Hello! My name is Aid.')
-print('I was created in 2020.')
-print('Please, remind me your name.')
-name = input()
-print('What a great name you have, ' + name + '!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
-rem3 = int(input())
-rem5 = int(input())
-rem7 = int(input())
-age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-print("Your age is " + str(age) + "; that's a good time to start programming!")
-print('Now I will prove to you that I can count to any number you want.')
-# read a number and count to it here
-count = int(input())
-i = 0
-while i <= count:
-    print(str(i) + " !")
-    i += 1
-print('Completed, have a nice day!')
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    questions = ['Why do we use methods?',
+                 "1. To repeat a statement multiple times.",
+                 "2. To decompose a program into several small subroutines.",
+                 "3. To determine the execution time of a program.",
+                 "4. To interrupt the execution of a program."
+                 ]
+    for n in questions:
+        print(n)
+    answear = int(input())
+    while answear != 2:
+        print('Please, try again.')
+        answear = int(input())
+    if answear == 2:
+        print('Completed, have a nice day!')
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Bilal', '2020') 
+remind_name()
+guess_age()
+count()
+test()
+end()
